e66c8ce023f228421fd4f50abf9026b1f51575df
Collin Martinez <102224274+CollinM5@users.noreply.github.com>
2024-06-17 09:06:58 +0200

    Ajout de la modal de contact animé

--- assets/js/contact-modal-script.js
+++ assets/js/contact-modal-script.js
@@ -3,11 +3,14 @@ const contactLink = document.getElementById("menu-item-26");
 
 
 contactLink.addEventListener("click", () => {
-    modalBackground.className = "open";
+    modalBackground.className = "open display";
 })
 
 modalBackground.addEventListener("click", (e) => {
     if(e.target.id == "modal-background"){
-        modalBackground.className = "close";
+        modalBackground.className = "close display";
+        setTimeout(() => {
+            modalBackground.className = "close displayNone";
+        }, "500");
     }
 })
